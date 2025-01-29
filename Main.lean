3b3e1f3b91aab0ec961640a600a54cd0247a7b87
zongjingli <lizongjing997@gmail.com>
2025-01-29 14:23:35 -0500

    write a simple proof and basic algebraic structures

--- Main.lean
+++ Main.lean
@@ -8,4 +8,39 @@ theorem hello_world (a b c : Nat)
 
 theorem foo (a : Nat) : a + 1 = Nat.succ a := by rfl
 
-#eval "Scourgeholmes"
+
+variable (a b c : ℝ)
+variable (p q r: Prop)
+
+theorem t : p → q → p := fun (hp : p) => fun (hq : q) => hp
+
+theorem t2 (f : p → q) (g : q → r) : p → r :=
+  fun (hp : p) => g (f hp)
+
+theorem triv (a b c : Nat)
+  : a + b + c = a + c + b :=
+  by rw [
+    add_assoc,
+    add_comm b,
+    add_assoc,
+  ]
+
+
+
+#check t2
+
+#eval
+
+/-
+潜入萨墓时他已经按耐不住嘴边疯狂的笑容
+他的一生都因为弱小而被操纵
+他是这样在泥泞中匍匐爬行,没有尊严,没有人格,没有一切
+他就像蛆虫一样丑陋,令人嫌恶,任人宰割
+没有什么是不可以背叛的,没有什么是值得信仰的
+他的内心从很早开始就已经空无一物了
+他所期待的也不过是有一天
+蛆虫也能长出站立的双腿，再长出钢铁般的拳头,拥有无可匹敌的力量
+那时他是不是也可以去撕咬他人的血肉，吞噬他人的骨头，操纵他人的人生
+让世间所有的生灵都感受他曾经的痛苦
+让所有生灵都明白弱小即是原罪，他们活该被自己踏入尘土
+-/
--- Soulforge/Basic.lean
+++ Soulforge/Basic.lean
@@ -1,1 +1,45 @@
-def hello := "world"
+def package := "Malkyriss"
+
+def Set (α : Type) := α → Prop
+
+
+structure Magma (carrier : Type) where
+  op : carrier → carrier → carrier
+
+structure Semigroup (carrier : Type) extends Magma carrier where
+  assoc : (a b c : carrier) → op (op a b) c = op a (op b c)
+
+structure Monoid (carrier : Type) extends Semigroup carrier where
+  id : carrier
+  op_id : (a : carrier) → op id a = a
+  id_op : (a : carrier) → op a id = a
+
+structure Group (carrier : Type) extends Monoid carrier where
+  inv : carrier → carrier
+  op_inv : (a : carrier) → op a (inv a) = id
+  inv_op : (a : carrier) → op (inv a) a = id
+
+structure AbelianGroup (carrier : Type) extends Group carrier where
+  op_commute : (a b : carrier) → op a b = op b a
+
+structure Ring (carrier : Type) extends AbelianGroup carrier where
+  mul : carrier → carrier → carrier
+  mul_assoc : ∀ a b c : carrier, mul a (mul b c) = mul (mul a b) c
+  one : carrier
+  id_equal : one = id
+  mul_one : ∀ a : carrier, mul a one = mul one a
+  one_mul : ∀ a : carrier, mul one a = mul a one
+  left_distrib : ∀ a x y : carrier, mul a (op x y) = op (mul a x) (mul a y)
+  right_distrib : ∀ x y a : carrier, mul (op x y) a = op (mul x a) (mul y a)
+
+structure CommutativeRing (carrier : Type) extends Ring carrier where
+  mul_commute : ∀ a b : carrier, mul a b = mul b a
+
+structure Field (carrier : Type) extends CommutativeRing carrier where
+  mul_inv : carrier → carrier
+  mul_by_inv : ∀ a : carrier, mul a (mul_inv a) = one
+  inv_by_mul : ∀ a : carrier, mul (mul_inv a) a = one
+
+theorem commutative_group_op (G : AbelianGroup carrier) (a b : carrier) :
+  G.op a b = G.op b a :=
+G.op_commute a b
